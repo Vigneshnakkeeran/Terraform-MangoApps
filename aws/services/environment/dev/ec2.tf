b4d6279e2e2a7cac21f9244ce559c3a0d9b3dd45
Adarshashok <adarshashok.k@cloudifyops.com>
2024-03-07 17:29:56 +0530

    modified:   aws/services/environment/dev/ec2.tf

--- aws/services/environment/dev/ec2.tf
+++ aws/services/environment/dev/ec2.tf
@@ -2,13 +2,11 @@ module "ec2_instance" {
   source  = "../../../modules/ec2"
 
   name = "${var.client_name}-${var.environment}-bastion-host"
-
   instance_type          = var.bastion_instance_type
   key_name               = var.asg_key_name
   monitoring             = true
   vpc_security_group_ids = [module.bastion_security_group.security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
-
   tags = {
     Created_by  = "Terraform"
     Client      = var.client_name

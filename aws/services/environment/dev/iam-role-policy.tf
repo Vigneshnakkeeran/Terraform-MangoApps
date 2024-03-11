de33872f9117ea7e1fdb75ff2963f80cde4bae2b
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-11 11:04:51 +0530

    Create iam-role-policy.tf

---
+++ aws/services/environment/dev/iam-role-policy.tf
@@ -0,0 +1,31 @@
+module "iam_role_with_policies" {
+  source = "./../../../../aws/modules/IAM-Role"
+
+  role_name           = "example-role"
+  assume_role_policy  = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"]
+  custom_policy_name  = "example-custom-policy"
+  custom_policy       = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "logs:CreateLogGroup",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}

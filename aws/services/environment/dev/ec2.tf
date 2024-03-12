19825f46f9d346fb0107cf480f36c66ffd19aaf7
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-12 17:01:49 +0530

    Update ec2.tf

--- aws/services/environment/dev/ec2.tf
+++ aws/services/environment/dev/ec2.tf
@@ -1,15 +1,88 @@
-module "ec2_instance" {
-  source  = "../../../modules/ec2"
+data "aws_availability_zones" "available" {}
 
-  name = "${var.client_name}-${var.environment}-bastion-host"
-  instance_type          = var.bastion_instance_type
-  key_name               = var.asg_key_name
-  monitoring             = true
-  vpc_security_group_ids = [module.bastion_security_group.security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
-  tags = {
-    Created_by  = "Terraform"
-    Client      = var.client_name
-    Environment = var.environment
+locals {
+  user_data = <<-EOT
+    #!/bin/bash
+    hostname "${instance_name}"
+    echo "${instance_name}" > /etc/hostname
+  EOT
+}
+
+################################################################################
+# EC2 Module
+################################################################################
+
+module "ec2_bastion" {
+  source = "../../"
+
+  name =  "${var.client_name}-${var.environment}-bastion-host"
+
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t3a.medium" # used to set core count below
+  availability_zone           = 
+  subnet_id                   = 
+  vpc_security_group_ids      = 
+  placement_group             = aws_placement_group.web.id
+  associate_public_ip_address = true
+
+  create_iam_instance_profile = true
+  iam_role_description        = "IAM role for EC2 instance"
+  iam_role_policies = {
+    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
+  }
+
+  # only one of these can be enabled at a time
+  hibernation = true
+  # enclave_options_enabled = true
+
+  user_data_base64            = <<-EOF
+                                  #!/bin/bash
+                                  hostname "${module.ec2_bastion.name}"
+                                  echo "${module.ec2_bastion.name}" > /etc/hostname
+                                  # Add any additional commands here
+                                  EOF
+  user_data_replace_on_change = true
+
+  cpu_options = {
+    core_count       = 2
+    threads_per_core = 1
+  }
+  enable_volume_tags = false
+  root_block_device = [
+    {
+      encrypted   = true
+      volume_type = "gp3"
+      throughput  = 200
+      volume_size = 50
+      tags = {
+        Name = "my-root-block"
+      }
+    },
+  ]
+
+  ebs_block_device = [
+    {
+      device_name = "/dev/sdf"
+      volume_type = "gp3"
+      volume_size = 5
+      throughput  = 200
+      encrypted   = true
+      kms_key_id  = aws_kms_key.this.arn
+      tags = {
+        MountPoint = "/mnt/data"
+      }
+    }
+  ]
+
+  tags = local.tags
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-x86_64-gp2"]
   }
 }

32bb315f17d3d3efb96b2fa28289172530c34d00
Rohith B <161837256+rohith-balarama@users.noreply.github.com>
2024-03-12 16:05:33 +0530

    removed subnet id and group from network.tf

--- aws/services/environment/dev/network.tf
+++ aws/services/environment/dev/network.tf
@@ -7,14 +7,10 @@ module "vpc" {
 
   public_subnets                     = var.public_subnets
   private_subnets                    = var.private_subnets
-  database_subnets                   = var.database_subnets
-  create_database_subnet_group       = var.create_database_subnet_group
-  create_database_subnet_route_table = var.create_database_subnet_route_table
 
   enable_nat_gateway                = var.enable_nat_gateway
   single_nat_gateway                = var.single_nat_gateway
   one_nat_gateway_per_az            = var.one_nat_gateway_per_az
-  create_database_nat_gateway_route = var.create_database_nat_gateway_route
 
   create_igw = var.create_igw
 

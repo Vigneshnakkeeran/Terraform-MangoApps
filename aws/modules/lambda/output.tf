1ebfe1d33d5be655dece78ea3a1a40e40a51589d
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-13 14:55:05 +0530

    Create output.tf

---
+++ aws/modules/lambda/output.tf
@@ -0,0 +1,10 @@
+output "lambda_execution_role" {
+  description = "The ARN of the IAM role created for Lambda execution"
+  value       = aws_iam_role.lambda_execution_role.arn
+}
+
+output "lambda_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.my_lambda.arn
+}
+

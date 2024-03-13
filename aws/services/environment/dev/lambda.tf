9fc035eb1d74d4da40ab287cb1de29aa5b2442e0
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-13 14:49:47 +0530

    Create lambda.tf

---
+++ aws/services/environment/dev/lambda.tf
@@ -0,0 +1,8 @@
+module "my_lambda_module" {
+  source               = "./my-lambda-module"
+  lambda_function_name = "my-lambda-function"
+  role                 = module.lambda_execution_role.arn
+  runtime              = "python3.8"
+  handler              = "index.handler"
+  memory_size =          ""
+}

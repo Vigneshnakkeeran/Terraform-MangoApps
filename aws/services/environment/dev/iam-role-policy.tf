153fe2102d914bb417ca4ad564be4f906bc6cb5c
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-13 14:49:53 +0530

    Update iam-role-policy.tf

--- aws/services/environment/dev/iam-role-policy.tf
+++ aws/services/environment/dev/iam-role-policy.tf
@@ -29,3 +29,39 @@
 # }
 # EOF
 # }
+
+################### LAMBDA FUNCTION EXECUTION ROLE ####################
+
+module "lambda_execution_role" {
+  source = "./../../../../aws/modules/IAM-Role"
+  role_name           = "${var.lambda_function_name}_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
+#   custom_policy_name  = "example-custom-policy"
+#   custom_policy       = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": "logs:CreateLogGroup",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOF
+# }
+
+

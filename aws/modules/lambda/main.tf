81263b90b13cc6ce17647fb7ec141182cc6df18c
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-14 11:38:12 +0530

    Update main.tf

--- aws/modules/lambda/main.tf
+++ aws/modules/lambda/main.tf
@@ -6,7 +6,10 @@ resource "aws_lambda_function" "my_lambda" {
   runtime = var.runtime
   handler = var.handler
   role    = var.role
-#  memory_size = var.memory_size
+  memory_size = var.memory_size
+  ephemeral_storage {
+    size = var.ephemeral_storage_size # Min 512 MB and the Max 10240 MB
+  }
 
   environment {
     variables = {

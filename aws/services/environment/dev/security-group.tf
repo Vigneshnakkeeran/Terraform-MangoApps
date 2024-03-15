426b888135295fde345de082dcbdce6ad663a304
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-15 11:04:06 +0530

    Update security-group.tf

--- aws/services/environment/dev/security-group.tf
+++ aws/services/environment/dev/security-group.tf
@@ -14,6 +14,29 @@ module "bastion_security_group" {
   }
 }
 
+module "private_security_group" {
+  source                   = "../../../modules/security-group"
+  create_sg                = var.create_private_sg
+  description              = var.bastion_sg_description
+  vpc_id                   = module.vpc.vpc_id
+  name                     = "${var.client_name}-${var.environment}-private-sg"
+  ingress_with_source_security_group_id = [
+  {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    description = "SSH Access to instances"
+    source_security_group_id = "${module.bastion_security_group.security_group_id}"
+  }
+  ]
+  egress_with_cidr_blocks = var.asg_egress_with_cidr_blocks
+  
+  tags = {
+    Created_by = "Terraform"
+    Client     = var.client_name
+    Env        = var.environment
+  }
+}
 # ASG Security group
 module "asg_security_group" {
   source                   = "../../../modules/security-group"

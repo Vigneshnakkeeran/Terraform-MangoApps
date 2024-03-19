34d2a47589cf95aec9fb76b80682eb03434ce198
Rohith B <161837256+rohith-balarama@users.noreply.github.com>
2024-03-19 15:14:53 +0530

    Update ec2.tf

--- aws/services/environment/dev/ec2.tf
+++ aws/services/environment/dev/ec2.tf
@@ -62,31 +62,31 @@ module "ec2_bastion" {
 
 ################ EC2 In Private Subnet ###################
 
-module "ec2_bastion" {
+module "ec2_backendserver" {
   source = "../../../modules/ec2"
 
   name =  local.instance_name
   ami                         = "ami-07d9b9ddc6cd8dd30"
   instance_type               = "t3a.medium" # used to set core count below
-  subnet_id                   = module.vpc.rpivate_subnets[0]
+  subnet_id                   = module.vpc.pivate_subnets[0]
   vpc_security_group_ids      = [module.private_security_group.security_group_id]
   associate_public_ip_address = false
   key_name        = var.asg_key_name
   create_iam_instance_profile = false
-  iam_role_description        = "IAM role for EC2 instance"
-  iam_role_policies = {
-    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
-  }
+  #iam_role_description        = "IAM role for EC2 instance"
+  #iam_role_policies = {
+  #  AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
+  #}
 
   # only one of these can be enabled at a time
   hibernation = false
   # enclave_options_enabled = true
 
-  user_data            = <<-EOF
-                                  #!/bin/bash
-                                  echo "${local.instance_name}" > /etc/hostname
-                                  # Add any additional commands here
-                                  EOF
+  #user_data            = <<-EOF
+  #                                #!/bin/bash
+  #                                echo "${local.instance_name}" > /etc/hostname
+  #                                # Add any additional commands here
+  #                                EOF
   user_data_replace_on_change = true
   enable_volume_tags = false
   root_block_device = [

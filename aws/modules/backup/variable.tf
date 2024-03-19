7361529ba1ea8e3465135da716791e6ce7dc4da7
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-19 09:42:25 +0530

    Create variable.tf

---
+++ aws/modules/backup/variable.tf
@@ -0,0 +1,42 @@
+variable "backup_vault_name" {
+  description = "The name of the backup vault."
+  type        = string
+}
+
+variable "backup_plan_name" {
+  description = "The name of the backup plan."
+  type        = string
+}
+
+variable "aurora_cluster_arn" {
+  description = "The ARN of the Aurora cluster to back up."
+  type        = string
+}
+
+variable "backup_rule_name" {
+  description = "The name of the backup rule."
+  type        = string
+}
+
+variable "schedule" {
+  description = "A CRON expression specifying when to perform the backup."
+  type        = string
+}
+
+variable "start_window" {
+  description = "The amount of time in minutes after a backup is scheduled before the backup job is canceled if it doesn't start successfully."
+  default     = 60
+  type        = number
+}
+
+variable "completion_window" {
+  description = "The amount of time in minutes after a backup starts that it must complete or it is canceled by AWS Backup."
+  default     = 120
+  type        = number
+}
+
+variable "recovery_point_tags" {
+  description = "Metadata that you can assign to help organize the resources that you create."
+  type        = map(string)
+  default     = {}
+}

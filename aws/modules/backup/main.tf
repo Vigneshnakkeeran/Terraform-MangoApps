b69fbedb0ec7fda4eff3fdd58ab3a17b9d554135
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-20 11:51:45 +0530

    Update main.tf

--- aws/modules/backup/main.tf
+++ aws/modules/backup/main.tf
@@ -1,5 +1,5 @@
-resource "aws_backup_vault" "example" {
-  name        = "example_backup_vault"
+resource "aws_backup_vault" "main" {
+  name        = "var.backup_vault_name"
   kms_key_arn = aws_kms_key.example.arn
 }
 
@@ -15,7 +15,7 @@ resource "aws_backup_plan" "main" {
     recovery_point_tags = var.recovery_point_tags
 
     lifecycle {
-      delete_after = 35
+      delete_after = var.delete_after
     }
 
     copy_action {
@@ -30,11 +30,11 @@ resource "aws_backup_plan" "main" {
 resource "aws_backup_selection" "main" {
   iam_role_arn = "arn:aws:iam::123456789012:role/service-role/AWSBackupDefaultServiceRole"
   plan_id      = aws_backup_plan.main.id
-  name         = "aurora-cluster-selection"
+  name         = var.backup_selection_name
 
   selection_tag {
     type  = "STRINGEQUALS"
     key   = "aws:backup:source-resource"
-    value = var.aurora_cluster_arn
+    value = var.target_backup_resource_arn
   }
 }

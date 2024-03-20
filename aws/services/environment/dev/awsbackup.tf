560d2ca3aab1ebb690b0776009314f5aac81e6ff
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-20 11:56:11 +0530

    Create awsbackup.tf

---
+++ aws/services/environment/dev/awsbackup.tf
@@ -0,0 +1,16 @@
+module "aws_backup" {
+  source              = "../../../../modules/backup"
+  backup_vault_name   = "my-vault"
+  backup_plan_name    = "aurora-backup-plan"
+  target_backup_resource_arn  = "arn:aws:rds:us-east-1:123456789012:cluster:my-aurora-cluster"
+  backup_rule_name    = "daily-backup"
+  schedule            = "cron(0 12 * * ? *)"
+  start_window        = ""
+  completion_window   = ""
+  recovery_point_tags = {
+    "Project" = "MyProject"
+  }
+  delete_after       = ""
+  destination_vault_arn = ""
+  backup_selection_name = ""
+}

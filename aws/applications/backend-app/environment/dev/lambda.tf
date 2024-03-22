13d67e2680b52a1a6649cb6ae2575f1229e73ed5
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-22 16:40:52 +0530

    Update lambda.tf

--- aws/applications/backend-app/environment/dev/lambda.tf
+++ aws/applications/backend-app/environment/dev/lambda.tf
@@ -1,6 +1,9 @@
  module "my_lambda_module" {
    source               = "./my-lambda-module"
+   source_path          =  ./lambda_function.rb
    lambda_function_name = "ses-lambda-function"  
+   layer_name           = "ses-lambda-layer"  
+   lambda_layer_path    = "./layer.zip"
    role                 = module.lambda_execution_role.arn
    runtime              = "ruby2.7"
    handler              = "index.handler"

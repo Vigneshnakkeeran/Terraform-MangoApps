c381ea21d1409359b64a18bfe29069b9bba8fccc
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-22 18:31:21 +0530

    Update output.tf

--- aws/applications/backend-app/environment/dev/output.tf
+++ aws/applications/backend-app/environment/dev/output.tf
@@ -22,6 +22,20 @@ output "s3_bucket_name" {
   value       = module.s3.s3_bucket_id
 }
 
+output "ses_s3_bucket_name" {
+  description = "The name of the bucket."
+  value       = module.ses_s3.s3_bucket_id
+}
+#output "s3_bucket_arn" {
+#  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
+#  value       = module.s3_bucket.s3_bucket_arn
+#}
+
+#output "s3_bucket_bucket_domain_name" {
+#  description = "The bucket domain name. Will be of format bucketname.s3.amazonaws.com."
+#  value       = module.s3_bucket.s3_bucket_bucket_domain_name
+#}
+
 # ######################## ASG ##########################
 
 output "autoscaling_group_id" {

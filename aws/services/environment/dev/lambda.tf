b4e1fbe97e5b0d832ac392c4d19d00ef6adcded0
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-22 15:52:23 +0530

    Update lambda.tf

--- aws/services/environment/dev/lambda.tf
+++ aws/services/environment/dev/lambda.tf
@@ -1,8 +1,8 @@
  module "my_lambda_module" {
    source               = "./my-lambda-module"
-   lambda_function_name = "ses-lambda-function"
+   lambda_function_name = "ses-lambda-function"  
    role                 = module.lambda_execution_role.arn
-   runtime              = "python3.8"
+   runtime              = "ruby2.7"
    handler              = "index.handler"
    memory_size =          "128"
    ephemeral_storage_size = "512"

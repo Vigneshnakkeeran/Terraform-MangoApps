065896ac7cb1e5e0e2717b6f4ecaa5b84c17e3f9
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-03-22 15:28:18 +0530

    Update iam-role-policy.tf

--- aws/services/environment/dev/iam-role-policy.tf
+++ aws/services/environment/dev/iam-role-policy.tf
@@ -32,36 +32,57 @@
 
 ################### LAMBDA FUNCTION EXECUTION ROLE ####################
 
-# module "lambda_execution_role" {
-#   source = "./../../../../aws/modules/IAM-Role"
-#   role_name           = "${var.lambda_function_name}_execution_role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "lambda.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
+ module "lambda_execution_role" {
+   source = "./../../../../aws/modules/IAM-Role"
+   role_name           = "${var.lambda_function_name}_ses_execution_role"
+   assume_role_policy = jsonencode({
+     Version = "2012-10-17"
+     Statement = [
+       {
+         Action = "sts:AssumeRole"
+         Effect = "Allow"
+         Principal = {
+           Service = "lambda.amazonaws.com"
+         }
+       },
+     ]
+   })
+ }
 #   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
-#   custom_policy_name  = "example-custom-policy"
-#   custom_policy       = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": "logs:CreateLogGroup",
-#       "Resource": "*"
-#     }
-#   ]
-# }
-# EOF
-# }
+   custom_policy_name  = "lambda-ses-custom-policy"
+   custom_policy       = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:CreateLogGroup",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "ses:SendRawEmail"
+                "sqs:ReceiveMessage",
+                "sqs:DeleteMessage",
+                "sqs:GetQueueAttributes",				
+            ],
+            "Resource": [
+                "arn:aws:s3:::<bucketName>/*",
+                "arn:aws:ses:<region>:<awsAccountId>:identity/*",
+				"arn:aws:sqs:<region>:<awsAccountId>:*"
+            ]
+        }
+    ]
+}
+ EOF
+ }
 
 

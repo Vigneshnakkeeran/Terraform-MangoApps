43c81f7b354a4b02f5baac2fe1ba6b557334e5bb
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-04-04 10:32:49 +0530

    Update cloudfront.tf

--- aws/applications/backend-app/environment/dev/cloudfront.tf
+++ aws/applications/backend-app/environment/dev/cloudfront.tf
@@ -87,199 +87,198 @@ module "cdn_post_endpoint" {
     acm_certificate_arn = "arn:aws:acm:us-east-1:730335460835:certificate/78aa3dce-d0f1-4532-982d-14ce22d88361"
     ssl_support_method  = "sni-only"
   }
-
 }
 
-# module "Static_Content_endpoint" {
-#   source = "../../../../modules/cloudfront"
+module "Static_Content_endpoint" {
+  source = "../../../../modules/cloudfront"
 
-#  # aliases = ["cdn.example.com"]
+ # aliases = ["cdn.example.com"]
 
-# #  comment             = "My awesome CloudFront"
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   price_class         = "PriceClass_All"
-#   retain_on_delete    = false
-#   wait_for_deployment = false
+#  comment             = "My awesome CloudFront"
+  enabled             = true
+  is_ipv6_enabled     = true
+  price_class         = "PriceClass_All"
+  retain_on_delete    = false
+  wait_for_deployment = false
 
-# #   create_origin_access_identity = true
-# #   origin_access_identities = {
-# #     s3_bucket_one = "My awesome CloudFront can access"
-# #   }
+#   create_origin_access_identity = true
+#   origin_access_identities = {
+#     s3_bucket_one = "My awesome CloudFront can access"
+#   }
 
-# #   logging_config = {
-# #     bucket = "logs-my-cdn.s3.amazonaws.com"
-# #   }
+#   logging_config = {
+#     bucket = "logs-my-cdn.s3.amazonaws.com"
+#   }
 
-#   origin = {
-#     Static_Content_endpoint = {
-#       domain_name = "hub.mangoapps-test-terraform.com"
-#       # custom_origin_config = {
-#       #   http_port              = 80
-#       #   https_port             = 443
-#       #   origin_protocol_policy = "match-viewer"
-#       #   origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-#       # }
+  origin = {
+    Static_Content_endpoint = {
+      domain_name = "hub.mangoapps-test-terraform.com"
+      custom_origin_config = {
+        http_port              = 80
+        https_port             = 443
+        origin_protocol_policy = "match-viewer"
+        origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      }
+    }
+
+    # s3_one = {
+    #   domain_name = "my-s3-bycket.s3.amazonaws.com"
+    #   s3_origin_config = {
+    #     origin_access_identity = "s3_bucket_one"
+    #   }
+    # }
+  }
+
+  default_cache_behavior = {
+    target_origin_id           = "Static_Content_endpoint"
+    viewer_protocol_policy     = "allow-all"
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods  = ["GET", "HEAD"]
+    cache_policy_name           = "Managed-CachingOptimized"
+    compress        = true
+    use_forwarded_values       = false
+    # query_string = true
+    # headers      = ["Origin"]
+    # cookies_forward = "none"
+    min_ttl     = 1
+    default_ttl = 604800
+    max_ttl     = 2592000
+#     lambda_function_association = {
+
+#       # Valid keys: viewer-request, origin-request, viewer-response, origin-response
+#       viewer-request = {
+#         lambda_arn   = module.qaLamdaAdge-viewer-request.lambda_function_qualified_arn
+# #        include_body = false
+#       }
+
+#       origin-request = {
+#         lambda_arn = module.qaLamdaAdge-origin-request.lambda_function_qualified_arn
+# #        include_body = false      
+#       }
+#       viewer-response = {
+#         lambda_arn = module.qaLamdaAdge-viewer-response.lambda_function_qualified_arn
+# #        include_body = false
+#       }
 #     }
+  }
 
-#     # s3_one = {
-#     #   domain_name = "my-s3-bycket.s3.amazonaws.com"
-#     #   s3_origin_config = {
-#     #     origin_access_identity = "s3_bucket_one"
-#     #   }
-#     # }
+#   ordered_cache_behavior = [
+#     {
+#       path_pattern           = "/static/*"
+#       target_origin_id       = "s3_one"
+#       viewer_protocol_policy = "redirect-to-https"
+
+#       allowed_methods = ["GET", "HEAD", "OPTIONS"]
+#       cached_methods  = ["GET", "HEAD"]
+#       compress        = true
+#       query_string    = true
+#     }
+#   ]
+
+  viewer_certificate = {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:730335460835:certificate/78aa3dce-d0f1-4532-982d-14ce22d88361"
+    ssl_support_method  = "sni-only"
+  }
+}
+
+locals {
+  domain_name = "mangoapps-test-terraform.com" # trimsuffix(data.aws_route53_zone.this.name, ".")
+  subdomain   = "awscloudfront"
+}
+
+module "Mango_Media_endpoint" {
+  source = "../../../../modules/cloudfront"
+#  depends_on          = [module.Mango_Media_endpoint_s3_bucket]
+
+  aliases = ["${local.subdomain}.${local.domain_name}"]
+
+#  comment             = "My awesome CloudFront"
+  enabled             = true
+  is_ipv6_enabled     = true
+  price_class         = "PriceClass_All"
+  retain_on_delete    = false
+  wait_for_deployment = false
+
+  create_origin_access_identity = true
+  origin_access_identities = {
+    s3_bucket_mango_media_endpoint = "s3_bucket_mango_media_endpoint-cloudfront-origin"
+  }
+
+#   logging_config = {
+#     bucket = "logs-my-cdn.s3.amazonaws.com"
 #   }
 
-#   default_cache_behavior = {
-#     target_origin_id           = "Static_Content_endpoint"
-#     viewer_protocol_policy     = "allow-all"
+  origin = {
+    Mango_Media_endpoint = {
+      domain_name = "${module.Mango_Media_endpoint_s3_bucket.s3_bucket_bucket_regional_domain_name}"
+      s3_origin_config = {
+        origin_access_identity = "s3_bucket_mango_media_endpoint"
+      }
+      # custom_origin_config = {
+      #   http_port              = 80
+      #   https_port             = 443
+      #   origin_protocol_policy = "https-only"
+      #   origin_ssl_protocols   = ["TLSv1.2"]
+      # }
+    }
 
-#     allowed_methods = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
-#     cached_methods  = ["GET", "HEAD"]
-#     cache_policy_name           = "Managed-CachingOptimized"
-#     compress        = true
-#     use_forwarded_values       = false
-#     # query_string = true
-#     # headers      = ["Origin"]
-#     # cookies_forward = "none"
-#     min_ttl     = 1
-#     default_ttl = 604800
-#     max_ttl     = 2592000
-# #     lambda_function_association = {
+    # s3_one = {
+    #   domain_name = "s3_bucket_bucket_regional_domain_name" #module.s3_one.s3_bucket_bucket_regional_domain_name
 
-# #       # Valid keys: viewer-request, origin-request, viewer-response, origin-response
-# #       viewer-request = {
-# #         lambda_arn   = module.qaLamdaAdge-viewer-request.lambda_function_qualified_arn
-# # #        include_body = false
-# #       }
+    # }
+  }
 
-# #       origin-request = {
-# #         lambda_arn = module.qaLamdaAdge-origin-request.lambda_function_qualified_arn
-# # #        include_body = false      
-# #       }
-# #       viewer-response = {
-# #         lambda_arn = module.qaLamdaAdge-viewer-response.lambda_function_qualified_arn
-# # #        include_body = false
-# #       }
-# #     }
-#   }
+  default_cache_behavior = {
+    target_origin_id           = "Mango_Media_endpoint"
+    viewer_protocol_policy     = "allow-all"
 
-# #   ordered_cache_behavior = [
-# #     {
-# #       path_pattern           = "/static/*"
-# #       target_origin_id       = "s3_one"
-# #       viewer_protocol_policy = "redirect-to-https"
+    allowed_methods = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods  = ["GET", "HEAD"]
+#    cache_policy_name           = "Managed-CachingOptimized"
+#    compress        = true
+    use_forwarded_values       = true
+    query_string = true
+    query_string_cache_keys = ["bucket_name"]
+#    headers      = ["Origin"]
+    cookies_forward = "none"
+    min_ttl     = 2592000
+    default_ttl = 2592000
+    max_ttl     = 31536000
+    lambda_function_association = {
 
-# #       allowed_methods = ["GET", "HEAD", "OPTIONS"]
-# #       cached_methods  = ["GET", "HEAD"]
-# #       compress        = true
-# #       query_string    = true
-# #     }
-# #   ]
+      # Valid keys: viewer-request, origin-request, viewer-response, origin-response
+      viewer-request = {
+        lambda_arn   = "${module.ma-media-auth-service.lambda_function_qualified_arn}"
+#        include_body = false
+      }
 
-#   viewer_certificate = {
-#     acm_certificate_arn = "arn:aws:acm:us-east-1:730335460835:certificate/78aa3dce-d0f1-4532-982d-14ce22d88361"
-#     ssl_support_method  = "sni-only"
-#   }
-# }
+      origin-request = {
+        lambda_arn = "${module.add-origin-cache-control-lambda-funtion.lambda_function_qualified_arn}"
+#        include_body = false      
+      }
+      viewer-response = {
+        lambda_arn = "${module.multi-s3-origin-request-lambda-function.lambda_function_qualified_arn}"
+#        include_body = false
+      }
+    }
+  }
 
-# locals {
-#   domain_name = "mangoapps-test-terraform.com" # trimsuffix(data.aws_route53_zone.this.name, ".")
-#   subdomain   = "awscloudfront"
-# }
+  # ordered_cache_behavior = [
+  #   {
+  #     path_pattern           = "/static/*"
+  #     target_origin_id       = "s3_one"
+  #     viewer_protocol_policy = "redirect-to-https"
 
-# # module "Mango_Media_endpoint" {
-# #   source = "../../../../modules/cloudfront"
-# # #  depends_on          = [module.Mango_Media_endpoint_s3_bucket]
+  #     allowed_methods = ["GET", "HEAD", "OPTIONS"]
+  #     cached_methods  = ["GET", "HEAD"]
+  #     compress        = true
+  #     query_string    = true
+  #   }
+  #]
 
-# #   aliases = ["${local.subdomain}.${local.domain_name}"]
-
-# # #  comment             = "My awesome CloudFront"
-# #   enabled             = true
-# #   is_ipv6_enabled     = true
-# #   price_class         = "PriceClass_All"
-# #   retain_on_delete    = false
-# #   wait_for_deployment = false
-
-# #   create_origin_access_identity = true
-# #   origin_access_identities = {
-# #     s3_bucket_mango_media_endpoint = "s3_bucket_mango_media_endpoint-cloudfront-origin"
-# #   }
-
-# # #   logging_config = {
-# # #     bucket = "logs-my-cdn.s3.amazonaws.com"
-# # #   }
-
-# #   origin = {
-# #     Mango_Media_endpoint = {
-# #       domain_name = "${module.Mango_Media_endpoint_s3_bucket.s3_bucket_bucket_regional_domain_name}"
-# #       s3_origin_config = {
-# #         origin_access_identity = "s3_bucket_mango_media_endpoint"
-# #       }
-# #       # custom_origin_config = {
-# #       #   http_port              = 80
-# #       #   https_port             = 443
-# #       #   origin_protocol_policy = "https-only"
-# #       #   origin_ssl_protocols   = ["TLSv1.2"]
-# #       # }
-# #     }
-
-# #     # s3_one = {
-# #     #   domain_name = "s3_bucket_bucket_regional_domain_name" #module.s3_one.s3_bucket_bucket_regional_domain_name
-
-# #     # }
-# #   }
-
-# #   default_cache_behavior = {
-# #     target_origin_id           = "Mango_Media_endpoint"
-# #     viewer_protocol_policy     = "allow-all"
-
-# #     allowed_methods = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
-# #     cached_methods  = ["GET", "HEAD"]
-# # #    cache_policy_name           = "Managed-CachingOptimized"
-# # #    compress        = true
-# #     use_forwarded_values       = true
-# #     query_string = true
-# #     query_string_cache_keys = ["bucket_name"]
-# # #    headers      = ["Origin"]
-# #     cookies_forward = "none"
-# #     min_ttl     = 2592000
-# #     default_ttl = 2592000
-# #     max_ttl     = 31536000
-# #     lambda_function_association = {
-
-# #       # Valid keys: viewer-request, origin-request, viewer-response, origin-response
-# #       viewer-request = {
-# #         lambda_arn   = "${module.ma-media-auth-service.lambda_function_qualified_arn}"
-# # #        include_body = false
-# #       }
-
-# #       origin-request = {
-# #         lambda_arn = "${module.add-origin-cache-control-lambda-funtion.lambda_function_qualified_arn}"
-# # #        include_body = false      
-# #       }
-# #       viewer-response = {
-# #         lambda_arn = "${module.multi-s3-origin-request-lambda-function.lambda_function_qualified_arn}"
-# # #        include_body = false
-# #       }
-# #     }
-# #   }
-
-# #   ordered_cache_behavior = [
-# #     {
-# #       path_pattern           = "/static/*"
-# #       target_origin_id       = "s3_one"
-# #       viewer_protocol_policy = "redirect-to-https"
-
-# #       allowed_methods = ["GET", "HEAD", "OPTIONS"]
-# #       cached_methods  = ["GET", "HEAD"]
-# #       compress        = true
-# #       query_string    = true
-# #     }
-# #   ]
-
-# #   viewer_certificate = {
-# #     acm_certificate_arn = "arn:aws:acm:us-east-1:730335460835:certificate/78aa3dce-d0f1-4532-982d-14ce22d88361"
-# #     ssl_support_method  = "sni-only"
-# #   }
-# # }
+  viewer_certificate = {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:730335460835:certificate/78aa3dce-d0f1-4532-982d-14ce22d88361"
+    ssl_support_method  = "sni-only"
+  }
+}

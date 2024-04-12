76d81368bd13876ab9123e4fb319c1e32eac9a18
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-04-12 14:23:33 +0530

    Update main.tf

--- aws/modules/backup/main.tf
+++ aws/modules/backup/main.tf
@@ -1,36 +1,104 @@
-resource "aws_backup_vault" "main" {
-  name        = var.backup_vault_name
-  kms_key_arn = aws_kms_key.example.arn
+# AWS Backup vault
+resource "aws_backup_vault" "ab_vault" {
+  count         = var.enabled && var.vault_name != null ? 1 : 0
+  name          = var.vault_name
+  kms_key_arn   = var.vault_kms_key_arn
+  force_destroy = var.vault_force_destroy
+  tags          = var.tags
 }
 
-resource "aws_backup_plan" "main" {
-  name = var.backup_plan_name
+# AWS Backup vault lock configuration
+resource "aws_backup_vault_lock_configuration" "ab_vault_lock_configuration" {
+  count               = var.locked && var.vault_name != null ? 1 : 0
+  backup_vault_name   = aws_backup_vault.ab_vault[0].name
+  changeable_for_days = var.changeable_for_days
+  max_retention_days  = var.max_retention_days
+  min_retention_days  = var.min_retention_days
+}
 
-  rule {
-    rule_name           = var.backup_rule_name
-    target_vault_name   = aws_backup_vault.main.name
-    schedule            = var.schedule
-    start_window        = var.start_window
-    completion_window   = var.completion_window
-    recovery_point_tags = var.recovery_point_tags
+# AWS Backup plan
+resource "aws_backup_plan" "ab_plan" {
+  count = var.enabled && length(local.rules) > 0 ? 1 : 0
+  name  = var.plan_name
 
-    lifecycle {
-      delete_after = var.delete_after
-    }
+  # Rules
+  dynamic "rule" {
+    for_each = local.rules
+    content {
+      rule_name                = lookup(rule.value, "name", null)
+      target_vault_name        = lookup(rule.value, "target_vault_name", null) != null ? rule.value.target_vault_name : var.vault_name != null ? aws_backup_vault.ab_vault[0].name : "Default"
+      schedule                 = lookup(rule.value, "schedule", null)
+      start_window             = lookup(rule.value, "start_window", null)
+      completion_window        = lookup(rule.value, "completion_window", null)
+      enable_continuous_backup = lookup(rule.value, "enable_continuous_backup", null)
+      recovery_point_tags      = length(lookup(rule.value, "recovery_point_tags", {})) == 0 ? var.tags : lookup(rule.value, "recovery_point_tags")
 
-    copy_action {
-      destination_vault_arn = var.destination_vault_arn
-      lifecycle {
-        delete_after = var.copy_retention
+      # Lifecycle
+      dynamic "lifecycle" {
+        for_each = length(lookup(rule.value, "lifecycle", {})) == 0 ? [] : [lookup(rule.value, "lifecycle", {})]
+        content {
+          cold_storage_after = lookup(lifecycle.value, "cold_storage_after", 0)
+          delete_after       = lookup(lifecycle.value, "delete_after", 90)
+        }
+      }
+
+      # Copy action
+      dynamic "copy_action" {
+        for_each = lookup(rule.value, "copy_actions", [])
+        content {
+          destination_vault_arn = lookup(copy_action.value, "destination_vault_arn", null)
+
+          # Copy Action Lifecycle
+          dynamic "lifecycle" {
+            for_each = length(lookup(copy_action.value, "lifecycle", {})) == 0 ? [] : [lookup(copy_action.value, "lifecycle", {})]
+            content {
+              cold_storage_after = lookup(lifecycle.value, "cold_storage_after", 0)
+              delete_after       = lookup(lifecycle.value, "delete_after", 90)
+            }
+          }
+        }
       }
     }
   }
+
+  # Advanced backup setting
+  dynamic "advanced_backup_setting" {
+    for_each = var.windows_vss_backup ? [1] : []
+    content {
+      backup_options = {
+        WindowsVSS = "enabled"
+      }
+      resource_type = "EC2"
+    }
+  }
+
+  # Tags
+  tags = var.tags
+
+  # First create the vault if needed
+  depends_on = [aws_backup_vault.ab_vault]
 }
 
-resource "aws_backup_selection" "example" {
-  iam_role_arn = "arn:aws:iam::123456789012:role/service-role/AWSBackupDefaultServiceRole"
-  name         = var.backup_selection_name
-  plan_id      = aws_backup_plan.main.id
+locals {
 
-  resources = var.target_backup_resource_arn
+  # Rule
+  rule = var.rule_name == null ? [] : [
+    {
+      name              = var.rule_name
+      target_vault_name = var.vault_name != null ? var.vault_name : "Default"
+      schedule          = var.rule_schedule
+      start_window      = var.rule_start_window
+      completion_window = var.rule_completion_window
+      lifecycle = var.rule_lifecycle_cold_storage_after == null ? {} : {
+        cold_storage_after = var.rule_lifecycle_cold_storage_after
+        delete_after       = var.rule_lifecycle_delete_after
+      }
+      enable_continuous_backup = var.rule_enable_continuous_backup
+      recovery_point_tags      = var.rule_recovery_point_tags
+    }
+  ]
+
+  # Rules
+  rules = concat(local.rule, var.rules)
+
 }

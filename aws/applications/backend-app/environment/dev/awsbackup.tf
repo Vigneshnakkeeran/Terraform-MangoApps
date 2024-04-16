31735c160179985b3849d47c1b4ee4b5c1038c84
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-04-16 10:25:25 +0530

    Update awsbackup.tf

--- aws/applications/backend-app/environment/dev/awsbackup.tf
+++ aws/applications/backend-app/environment/dev/awsbackup.tf
@@ -1,21 +1,70 @@
-# module "aws_backup" {
-#   source              = "../../../../modules/backup"
-#   backup_vault_name   = "my-vault"
-#   backup_plan_name    = "aurora-backup-plan"
-#   target_backup_resource_arn  = "arn:aws:rds:us-east-1:123456789012:cluster:my-aurora-cluster"
-#   backup_rule_name    = "daily-backup"
-#   schedule            = "cron(0 12 * * ? *)"
-#   start_window        = ""
-#   completion_window   = ""
-#   recovery_point_tags = {
-#     "Project" = "MyProject"
-#   }
-#   delete_after       = ""
-#   destination_vault_arn = module.destination_vault_aws_backup.arn
-#   backup_selection_name = ""
-# }
+# module "destination_vault" {
 
-# module "destination_vault_aws_backup" {
-#   source              = "../../../../modules/backup"
-#   backup_vault_name   = "my-destination-vault"
-# }
+#   source = "../../../../modules/backup"
+#   providers = {
+#     aws = aws.useast1
+#   }
+
+#   # Vault
+#   vault_name = "destination-vault"
+
+#  }
+
+resource "aws_backup_vault" "destination_vault" {
+  provider = aws.useast1
+  name     = "destination-vault"
+}
+
+module "aws_backup_selection" {
+
+  source = "../../../../modules/backup"
+
+  # Vault
+  vault_name = "source-vault"
+
+  # Plan
+  plan_name = "cross-region-selection-tags-plan"
+
+  # Multiple rules using a list of maps
+  rules = [
+    {
+      name                = "cross-region"
+      schedule            = "cron(0 0 * * ? *)"
+      target_vault_name   = "source-vault"             #module.destination_vault.vault_id
+      start_window        = 120
+      completion_window   = 360
+    #   lifecycle           = {}
+      copy_actions        = [
+        {
+          lifecycle = {
+            cold_storage_after = 0
+            delete_after       = 90
+          },
+          destination_vault_arn = "${aws_backup_vault.destination_vault.arn}"
+        }
+      ]
+    }
+  ]
+
+  # Multiple selections
+  #  - Selection-1: By tags: Environment = prod, Owner = devops
+  selections = [
+    {
+      name = "selection-1"
+      selection_tags = [
+        {
+          type  = "STRINGEQUALS"
+          key   = "Backup"
+          value = "True"
+        }
+      ]
+    }
+  ]
+
+#   tags = {
+#     Owner       = "devops"
+#     Environment = "prod"
+#     Terraform   = true
+#   }
+
+}

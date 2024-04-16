72c337df77331fdf259943e175654810db8b3ef7
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-04-16 10:24:26 +0530

    Update variable.tf

--- aws/modules/backup/variable.tf
+++ aws/modules/backup/variable.tf
@@ -1,64 +1,228 @@
-variable "backup_vault_name" {
-  description = "The name of the backup vault."
+#
+# AWS Backup vault
+#
+variable "vault_name" {
+  description = "Name of the backup vault to create. If not given, AWS use default"
   type        = string
+  default     = null
 }
 
-variable "backup_plan_name" {
-  description = "The name of the backup plan."
+variable "vault_kms_key_arn" {
+  description = "The server-side encryption key that is used to protect your backups"
   type        = string
+  default     = null
 }
 
-variable "target_backup_resource_arn" {
-  description = "The ARN of the Aurora cluster to back up."
-  type        = string
-}
-
-variable "backup_rule_name" {
-  description = "The name of the backup rule."
-  type        = string
-}
-
-variable "destination_vault_arn" {
-  description = "The AWS region to copy the backups to."
-  type        = string
-}
-
-variable "copy_retention" {
-  description = "The number of days to retain the copied backups in the destination region."
-  default     = 30
-  type        = number
-}
-
-variable "delete_after" {
-  description = "The number of days to retain the copied backups in the destination region."
-  default     = 30
-  type        = number
-}
-
-variable "backup_selection_name" {
-  description = "A CRON expression specifying when to perform the backup."
-  type        = string
-}
-
-variable "schedule" {
-  description = "A CRON expression specifying when to perform the backup."
-  type        = string
-}
-
-variable "start_window" {
-  description = "The amount of time in minutes after a backup is scheduled before the backup job is canceled if it doesn't start successfully."
-  default     = 60
-  type        = number
-}
-
-variable "completion_window" {
-  description = "The amount of time in minutes after a backup starts that it must complete or it is canceled by AWS Backup."
-  default     = 120
-  type        = number
-}
-
-variable "recovery_point_tags" {
-  description = "Metadata that you can assign to help organize the resources that you create."
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
   type        = map(string)
   default     = {}
+}
+
+variable "vault_force_destroy" {
+  description = "A boolean that indicates that all recovery points stored in the vault are deleted so that the vault can be destroyed without error."
+  type        = bool
+  default     = false
+}
+
+#
+# AWS Backup vault lock configuration
+#
+variable "locked" {
+  description = "Change to true to add a lock configuration for the backup vault"
+  type        = bool
+  default     = false
+}
+
+variable "changeable_for_days" {
+  description = "The number of days before the lock date. If omitted creates a vault lock in governance mode, otherwise it will create a vault lock in compliance mode"
+  type        = number
+  default     = null
+}
+
+variable "max_retention_days" {
+  description = "The maximum retention period that the vault retains its recovery points"
+  type        = number
+  default     = null
+}
+
+variable "min_retention_days" {
+  description = "The minimum retention period that the vault retains its recovery points"
+  type        = number
+  default     = null
+}
+
+#
+# AWS Backup plan
+#
+variable "plan_name" {
+  description = "The display name of a backup plan"
+  type        = string
+  default     = null
+}
+
+# Default rule
+variable "rule_name" {
+  description = "An display name for a backup rule"
+  type        = string
+  default     = null
+}
+
+variable "rule_schedule" {
+  description = "A CRON expression specifying when AWS Backup initiates a backup job"
+  type        = string
+  default     = null
+}
+
+variable "rule_start_window" {
+  description = "The amount of time in minutes before beginning a backup"
+  type        = number
+  default     = null
+}
+
+variable "rule_completion_window" {
+  description = "The amount of time AWS Backup attempts a backup before canceling the job and returning an error"
+  type        = number
+  default     = null
+}
+
+variable "rule_recovery_point_tags" {
+  description = "Metadata that you can assign to help organize the resources that you create"
+  type        = map(string)
+  default     = {}
+}
+
+# Rule lifecycle
+variable "rule_lifecycle_cold_storage_after" {
+  description = "Specifies the number of days after creation that a recovery point is moved to cold storage"
+  type        = number
+  default     = null
+}
+
+variable "rule_lifecycle_delete_after" {
+  description = "Specifies the number of days after creation that a recovery point is deleted. Must be 90 days greater than `cold_storage_after`"
+  type        = number
+  default     = null
+}
+
+# Rule copy action
+variable "rule_copy_action_lifecycle" {
+  description = "The lifecycle defines when a protected resource is copied over to a backup vault and when it expires."
+  type        = map(any)
+  default     = {}
+}
+
+variable "rule_copy_action_destination_vault_arn" {
+  description = "An Amazon Resource Name (ARN) that uniquely identifies the destination backup vault for the copied backup."
+  type        = string
+  default     = null
+}
+
+variable "rule_enable_continuous_backup" {
+  description = " Enable continuous backups for supported resources."
+  type        = bool
+  default     = false
+}
+
+
+# Rules
+variable "rules" {
+  description = "A list of rule maps"
+  type        = any
+  default     = []
+}
+
+# Selection
+variable "selection_name" {
+  description = "The display name of a resource selection document"
+  type        = string
+  default     = null
+}
+
+variable "selection_resources" {
+  description = "An array of strings that either contain Amazon Resource Names (ARNs) or match patterns of resources to assign to a backup plan"
+  type        = list(any)
+  default     = []
+}
+
+variable "selection_not_resources" {
+  description = "An array of strings that either contain Amazon Resource Names (ARNs) or match patterns of resources to exclude from a backup plan."
+  type        = list(any)
+  default     = []
+}
+
+variable "selection_conditions" {
+  description = "A map of conditions that you define to assign resources to your backup plans using tags."
+  type        = map(any)
+  default     = {}
+}
+
+variable "selection_tags" {
+  description = "List of tags for `selection_name` var, when using variable definition."
+  type        = list(any)
+  default     = []
+}
+
+# Selection
+variable "selections" {
+  description = "A list of selction maps"
+  type        = any
+  default     = []
+}
+
+variable "enabled" {
+  description = "Change to false to avoid deploying any AWS Backup resources"
+  type        = bool
+  default     = true
+}
+
+# Windows Backup parameter
+variable "windows_vss_backup" {
+  description = "Enable Windows VSS backup option and create a VSS Windows backup"
+  type        = bool
+  default     = false
+}
+
+#
+# Notifications
+#
+variable "notifications" {
+  description = "Notification block which defines backup vault events and the SNS Topic ARN to send AWS Backup notifications to. Leave it empty to disable notifications"
+  type        = any
+  default     = {}
+}
+
+#
+# IAM
+#
+variable "iam_role_arn" {
+  description = "If configured, the module will attach this role to selections, instead of creating IAM resources by itself"
+  type        = string
+  default     = null
+}
+
+variable "iam_role_name" {
+  description = "Allow to set IAM role name, otherwise use predefined default"
+  type        = string
+  default     = ""
+}
+
+#
+# AWS Backup Report Plan
+#
+variable "reports" {
+  description = "The default cache behavior for this distribution."
+  type = list(object({
+    name               = string
+    description        = optional(string, null)
+    formats            = optional(list(string), null)
+    s3_bucket_name     = string
+    s3_key_prefix      = optional(string, null)
+    report_template    = string
+    accounts           = optional(list(string), null)
+    organization_units = optional(list(string), null)
+    regions            = optional(list(string), null)
+    framework_arns     = optional(list(string), [])
+  }))
+  default = []
 }

bc43539409c4845464edf157363541c11a00bb72
Adarshashok <adarshashok.k@cloudifyops.com>
2024-05-03 17:27:24 +0530

    modified:   azure/services/environment/dev/activity_logs.tf

--- azure/services/environment/dev/activity_logs.tf
+++ azure/services/environment/dev/activity_logs.tf
@@ -5,7 +5,24 @@ resource "azurerm_log_analytics_workspace" "example" {
   resource_group_name = data.terraform_remote_state.dev_resource_group.outputs.dev_rg
 }
 
-resource "azurerm_monitor_activity_log_alert" "rg" {
+resource "azurerm_monitor_action_group" "log_analytics" {
+  name                = "mango-action-group02"
+  resource_group_name = data.terraform_remote_state.dev_resource_group.outputs.dev_rg
+  short_name          = "mango01"
+  location            = var.location
+  webhook_receiver {
+    name                    = "log_analytics-webhook"
+    service_uri             = "https://${azurerm_log_analytics_workspace.example.workspace_id}.ods.opinsights.azure.com/webhookreceiverid?api-version=2016-04-01"
+    use_common_alert_schema = true
+  }
+}
+
+
+#===================================================================
+# Set Monitoring and Log Analytics Workspace For VM
+#===================================================================
+
+resource "azurerm_monitor_activity_log_alert" "vm" {
   name                = "example-activitylogalert"
   resource_group_name = data.terraform_remote_state.dev_resource_group.outputs.dev_rg
   scopes              = [data.terraform_remote_state.dev_resource_group.outputs.dev_rg_id]
@@ -22,33 +39,9 @@ resource "azurerm_monitor_activity_log_alert" "rg" {
   }
 
   enabled = true
+  depends_on = [ module.linux ]
 }
 
-resource "azurerm_monitor_action_group" "log_analytics" {
-  name                = "mango-action-group02"
-  resource_group_name = data.terraform_remote_state.dev_resource_group.outputs.dev_rg
-  short_name          = "mango01"
-  location            = var.location
-  webhook_receiver {
-    name                    = "log_analytics-webhook"
-    service_uri             = "https://${azurerm_log_analytics_workspace.example.workspace_id}.ods.opinsights.azure.com/webhookreceiverid?api-version=2016-04-01"
-    use_common_alert_schema = true
-  }
-}
-
-# resource "azurerm_monitor_action_group_receiver" "log_analytics_receiver" {
-#   name                = azurerm_log_analytics_workspace.example.name
-#   action_group_id     = azurerm_monitor_action_group.log_analytics.id
-#   log_analytics       = {
-#     workspace_id = azurerm_log_analytics_workspace.example.id
-#     workspace_key = azurerm_log_analytics_workspace.example.primary_shared_key
-#   }
-# }
-
-
-#===================================================================
-# Set Monitoring and Log Analytics Workspace
-#===================================================================
 resource "azurerm_virtual_machine_extension" "vm01" {
   name                       = "test-OMSExtension"
   virtual_machine_id         =  module.linux.vm_id
@@ -68,4 +61,29 @@ resource "azurerm_virtual_machine_extension" "vm01" {
       "workspaceKey" : "${azurerm_log_analytics_workspace.example.primary_shared_key}"
     }
   PROTECTED_SETTINGS
+}
+
+
+#===================================================================
+# Set Monitoring and Log Analytics Workspace For AGW
+#===================================================================
+
+resource "azurerm_monitor_activity_log_alert" "agw" {
+  name                = "agw-activitylogalert"
+  resource_group_name = data.terraform_remote_state.dev_resource_group.outputs.dev_rg
+  scopes              = [data.terraform_remote_state.dev_resource_group.outputs.dev_rg_id]
+  description         = "This alert will monitor a specific rg updates."
+
+  criteria {
+    category          = "Recommendation"
+    resource_id       = module.application_gateway.application_gateway_id
+    operation_name    = "Microsoft.Network/applicationGateways/write"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+  }
+
+  enabled = true
+  depends_on = [ module.application_gateway ]
 }

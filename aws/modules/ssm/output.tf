2e58f6c1b52489ba14ac500d70436bdd3e9bf1b4
abhayjitK <161598361+abhayjitK@users.noreply.github.com>
2024-06-03 15:02:46 +0530

    Update output.tf

--- aws/modules/ssm/output.tf
+++ aws/modules/ssm/output.tf
@@ -1,15 +1,15 @@
-output "ssm_parameters" {
-  value = aws_ssm_parameter.this
+output "ssm_parameter_id" {
+  value = aws_ssm_parameter.this.id
 }
 
-output "ssm_documents" {
-  value = aws_ssm_document.this
+output "ssm_document_name" {
+  value = aws_ssm_document.this.name
 }
 
-output "ssm_maintenance_windows" {
-  value = aws_ssm_maintenance_window.this
+output "ssm_maintenance_window_id" {
+  value = aws_ssm_maintenance_window.this.id
 }
 
-output "ssm_maintenance_window_tasks" {
-  value = aws_ssm_maintenance_window_task.this
+output "ssm_maintenance_window_task_id" {
+  value = aws_ssm_maintenance_window_task.this.id
 }
